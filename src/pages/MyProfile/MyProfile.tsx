9ff0f981ce6bebdbf252147a38bcdc4c5b4aaeb0
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-08-11 23:21:17 +0900

    Feat: MyProfile, Writing 페이지 추가

---
+++ src/pages/MyProfile/MyProfile.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./MyProfile.scss";
+import { TextInput, Button } from "joseph-ui-kit";
+
+const MyProfile = () => {
+  return (
+    <div className="container ">
+      <div className="icon" />
+      <TextInput />
+      <Button />
+    </div>
+  );
+};
+
+export default MyProfile;
---
+++ src/pages/Writing/Writing.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { TextInput, TextArea, Button } from "joseph-ui-kit";
+
+const Writing = () => {
+  return (
+    <div>
+      <TextInput hideLabel hideWarn />
+      <Button />
+      <TextArea hideLabel hideWarn />
+      <Button />
+    </div>
+  );
+};
+
+export default Writing;
