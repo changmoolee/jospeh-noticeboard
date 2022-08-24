8dfdbd5316a318902e3abc4a05f75d6901303527
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-08-24 15:16:59 +0900

    Refactor: Post 컴포넌트에서 CommentContainer 컴포넌트 생성해서 분리

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,78 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styles from "./Post.module.scss";
-import { Button, TextInput } from "joseph-ui-kit";
-import { db } from "../../firebase";
-import { getAuth } from "firebase/auth";
-import { doc, updateDoc, getDoc } from "firebase/firestore";
-import { uuidv4 } from "@firebase/util";
-import Comment from "../Comment/Comment";
+import CommentContainer from "../CommentContainer/CommentContainer";
 
 const Post = ({ post }: any) => {
-  const [typedComment, setTypedComment] = useState("");
-  const [warnComment, setWarnComment] = useState("");
-  const [isOpenMoreComments, isSetOpenMoreComments] = useState(false);
-  const [comments, setComments] = useState([]);
-
-  const auth = getAuth();
-  const user = auth.currentUser;
-
-  const openMoreComment = () => {
-    isSetOpenMoreComments(true);
-  };
-
-  const closeComment = () => {
-    isSetOpenMoreComments(false);
-  };
-
-  const postComment = () => {
-    const commentId = uuidv4();
-    if (!typedComment) {
-      setWarnComment("아무것도 입력하지 않으셨습니다.");
-    } else {
-      setWarnComment("");
-
-      const commentRef = doc(db, "comment", post.postId);
-
-      updateDoc(commentRef, {
-        comments: [
-          ...comments,
-          {
-            commentId: commentId,
-            createdTime: new Date().toLocaleString(),
-            userId: user?.uid,
-            userImage: user?.photoURL,
-            user: user?.displayName,
-            commentContent: typedComment,
-          },
-        ],
-      })
-        .then(() => {
-          const comment = getDoc(doc(db, "comment", post.postId));
-
-          comment
-            .then((doc: any) => {
-              if (doc.data()?.comments) {
-                setComments(doc.data()?.comments);
-              }
-            })
-            .catch((err) => console.log(err));
-        })
-        .catch((err) => console.log(err));
-    }
-  };
-
-  useEffect(() => {
-    const comment = getDoc(doc(db, "comment", post.postId));
-
-    comment
-      .then((doc: any) => {
-        if (doc.data()?.comments) {
-          setComments(doc.data()?.comments);
-        }
-      })
-      .catch((err) => console.log(err));
-  }, [warnComment, post.postId]);
-
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -92,63 +22,16 @@ const Post = ({ post }: any) => {
             <div className={styles.nullImage} />
           )}
 
-          <div className={styles.nameAndTime}>
-            <div className={styles.username}>{post && post.userId}</div>
+          <div className={styles.nicknameAndTime}>
+            <div className={styles.userNickname}>
+              {post && post.userNickname}
+            </div>
             <div className={styles.createdTime}>{post && post.createdTime}</div>
           </div>
         </div>
       </div>
       <div className={styles.footer}>{post && post.content}</div>
-      <div className={styles.commentBox}>
-        <div className={styles.inputWrapper}>
-          <TextInput
-            width="100%"
-            placeholder="댓글을 입력해 주세요."
-            hideLabel
-            warn={warnComment}
-            maxLength={500}
-            onChange={(data) => setTypedComment(data.value)}
-          />
-        </div>
-        <Button
-          width="80px"
-          padding="0"
-          position="center"
-          name="댓글 달기"
-          onClick={postComment}
-        />
-      </div>
-      {comments?.length === 0 ? (
-        <div className={styles.noComment}>댓글이 없습니다.</div>
-      ) : comments?.length === 1 ? (
-        <Comment comment={comments[0]} />
-      ) : isOpenMoreComments ? (
-        <>
-          {comments.map((comment: any) => (
-            <Comment comment={comment} key={comment.commentId} />
-          ))}
-          <Button
-            kind="tertiary"
-            position="center"
-            width="100%"
-            padding="0"
-            name="댓글 감추기"
-            onClick={closeComment}
-          />
-        </>
-      ) : (
-        <>
-          <Comment comment={comments[0]} />
-          <Button
-            kind="tertiary"
-            position="center"
-            width="100%"
-            padding="0"
-            name={`댓글 ${comments.length - 1} 개 더보기`}
-            onClick={openMoreComment}
-          />
-        </>
-      )}
+      <CommentContainer post={post} />
     </div>
   );
 };
