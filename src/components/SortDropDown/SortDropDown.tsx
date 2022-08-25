5a950b62004d9cca7f6e5c7cf169c984f03f50de
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-08-25 22:46:57 +0900

    Feat: DropDown ui를 이용한 게시판 데이터 정렬 기능 추가

---
+++ src/components/SortDropDown/SortDropDown.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import styles from "./SortDropDown.module.scss";
+import { Button, DropDown } from "joseph-ui-kit";
+import { db } from "../../firebase";
+import { collection, getDocs, query, orderBy } from "firebase/firestore";
+
+const items = [
+  { id: "asc", value: "최신순" },
+  { id: "desc", value: "오래된순" },
+];
+
+const SortDropDown = ({ setIsLoading, setPosts }: any) => {
+  const [sortMode, setSortMode] = useState<"asc" | "desc">("asc");
+
+  const freeboardRef = collection(db, "freeboard");
+
+  const first = query(freeboardRef, orderBy("createdTime", sortMode));
+
+  useEffect(() => {
+    setPosts([]);
+    setIsLoading(true);
+
+    getDocs(first)
+      .then((res) => {
+        res.forEach((doc: any) => {
+          // doc.data() is never undefined for query doc snapshots
+          setPosts((prev: any) => [doc.data(), ...prev]);
+        });
+      })
+      .then(() => {
+        setIsLoading(false);
+      })
+      .catch((err) =>
+        console.log(err, "게시물 데이터를 불러오는데 실패했습니다.")
+      );
+  }, [sortMode]);
+
+  return (
+    <div className={styles.dropdownContainer}>
+      <DropDown
+        width="150px"
+        label="정렬"
+        hideWarn
+        items={items}
+        onChange={(data: any) => setSortMode(data.selectedItem.id)}
+      />
+    </div>
+  );
+};
+
+export default SortDropDown;
