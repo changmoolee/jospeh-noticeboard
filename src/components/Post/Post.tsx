8c7e3a1f2b8fbc30419e38f8a039697feaaa5c3e
CHANGMOO LEE <84559872+changmoolee@users.noreply.github.com>
2022-08-31 21:07:47 +0900

    Fix: firebase storage 대역폭 사용량을 줄이기 위해 nullImage 및 no-camera 에러이미지 정적이미지로 대체

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -42,10 +42,11 @@ const Post = ({ post }: PostProps) => {
               className={styles.contentImage}
               alt="contentImage"
               src={post.contentImage}
-              onError={(e: React.SyntheticEvent<HTMLImageElement>) =>
-                ((e.target as HTMLImageElement).src =
-                  "https://firebasestorage.googleapis.com/v0/b/joseph-noticeboard.appspot.com/o/no-camera.png?alt=media&token=8e62d94f-3465-45bb-8cce-15476c91b727")
-              }
+              onError={(e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+                (
+                  e.target as HTMLImageElement
+                ).src = require("../../assets/images/no-camera.png");
+              }}
             />
           </div>
         ) : null}
